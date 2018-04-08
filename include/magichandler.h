cd9cefdb0857a001956911d9dd89daa596e8bd0d
gavin <gavin_gr@msn.com>
2018-04-08 14:56:49 -0500

    all data inputted for Magic combos

--- include/magichandler.h
+++ include/magichandler.h
@@ -4,10 +4,38 @@
 #include "gamemap.h"
 #include "prop.h"	// for manipulating environment
 #include "actor.h"	// for dealing damage
+#include "mymath.h"
+
 #include "textconsole.h"
+
+#define AOETILES1			12
+#define MAGICPERCOMBO		5
+
+struct AoE {
+	Vector2i tiles[AOETILES1]; // 12 * (4 * 2) = 96
+}; // 96
+
+struct Combo {
+	// vectors from a "center" tile, which is guaranteed included
+	Vector2i tiles[MAGICPERCOMBO]; // 5 * (4 * 2) = 40
+	AoE aoe; // 96
+	char element; // 1
+	bool rotatable; // <1
+	bool reversable; // <1
+}; // 136 + <=2 + 1 ~= 138
+
+struct AllCombos {
+	Combo ice;
+	Combo fire; 
+	Combo water; 
+	Combo earth; 
+	Combo wood; 
+}; // 138 * 5 = 690
 
 class MagicHandler {
 private:
+	AllCombos combos;
+	AoE neutralaoe;
 
 public:
 	MagicHandler();
--- include/mymath.h
+++ include/mymath.h
@@ -6,4 +6,37 @@ struct Vector2i {
 	int y;
 };
 
+class MyMath {
+	static int manhattan(Vector2i pos1, Vector2i pos2) {
+		int dx = pos1.x - pos2.x;
+		if (dx < 0)
+			dx *= -1;
+		int dy = pos1.y - pos2.y;
+		if (dy < 0)
+			dy *= -1;
+		return dx + dy;
+	}
+
+	static Vector2i neg(Vector2i v) {
+		Vector2i r;
+		r.x = -1 * v.x;
+		r.y = -1 * v.y;
+		return r;
+	}
+
+	static Vector2i add(Vector2i v1, Vector2i v2) {
+		Vector2i r;
+		r.x = v1.x + v2.x;
+		r.y = v1.y + v2.y;
+		return r;
+	}
+
+	static Vector2i rot90(Vector2i v) {
+		Vector2i r;
+		r.x = -1 * v.y;
+		r.y = v.x;
+		return r;
+	}
+};
+
 #endif
